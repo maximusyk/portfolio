90a972bde0d7719dbf70847d1555e380c550d220
maximusyk <22usyk08@gmail.com>
2021-09-16 02:29:52 +0300

    add responsibility

--- index.js
+++ index.js
@@ -20,37 +20,75 @@ document.addEventListener("click", function (e) {
   burst.tune({ x: e.pageX, y: e.pageY }).replay();
 });
 
-$(".project-item")
-  .on("mouseover", function (e) {
-    $(this).children(".item-text-title").css({ opacity: 1 });
-  })
-  .on("mouseout", function (e) {
-    $(this).children(".item-text-title").css({ opacity: 0 });
-  })
-  .on("mousemove", function (e) {
-    var translateX =
-      ((e.pageX - $(this).offset().left) / $(this).width()) * 100;
-    var translateY =
-      ((e.pageY - $(this).offset().top) / $(this).height()) * 100;
+var input = $(".validate-input .input");
 
-    $(".item-image img").css({
-      "transform-origin": translateX + "% " + translateY + "%",
-    });
-  });
+$(".validate-form").on("submit", function () {
+  var check = true;
 
-// $("body").click(function (e) {
-//   $(".project-item").removeClass("clicked");
-// });
-
-$(".item-text-title").click(function (e) {
-  const clickedIndex = $(this).closest(".project-item").index();
-  $(".project-item").removeClass("clicked");
-
-  $(this).closest(".project-item").toggleClass("clicked");
-  if (clickedIndex - 3 >= 0) {
-    $(`.project-item:eq(${clickedIndex - 3})`).toggleClass("clicked");
+  for (var i = 0; i < input.length; i++) {
+    if (validate(input[i]) == false) {
+      showValidate(input[i]);
+      check = false;
+    }
   }
-  if (clickedIndex - 6 >= 0) {
-    $(`.project-item:eq(${clickedIndex - 6})`).toggleClass("clicked");
+
+  const inputs = document.querySelector(".contact-form");
+
+  if (check) {
+    console.log("Check");
+    Email.send({
+      Host: "smtp.gmail.com",
+      Username: "22usyk08@gmail.com",
+      Password: "mvamknkaowvqlpzs",
+      To: "22usyk08@gmail.com",
+      From: inputs.elements["email"].value,
+      Subject: "PORTFOLIO EMAIL",
+      Body:
+        inputs.elements["message"].value +
+        "<br>Client name: <strong>" +
+        inputs.elements["name"].value +
+        "</strong>",
+    }).then((message) => alert(message));
   }
+
+  return false;
 });
+
+$(".validate-form .input").each(function () {
+  $(this).focus(function () {
+    hideValidate(this);
+  });
+});
+
+function validate(input) {
+  if ($(input).attr("type") == "email" || $(input).attr("name") == "email") {
+    if (
+      $(input)
+        .val()
+        .trim()
+        .match(
+          /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+        ) == null
+    ) {
+      return false;
+    }
+  } else {
+    if ($(input).val().trim() == "") {
+      return false;
+    }
+  }
+}
+
+function showValidate(input) {
+  var thisAlert = $(input).parent();
+
+  $(thisAlert).addClass("alert-validate");
+}
+
+function hideValidate(input) {
+  var thisAlert = $(input).parent();
+
+  $(thisAlert).removeClass("alert-validate");
+}
+
+// e172fbad-be47-4646-84cf-6e52ad5dacdf
